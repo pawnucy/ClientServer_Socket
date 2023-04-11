090f9e1f512e923d5a401f516b4fb191561c1326
Paweł <pawelpuchalski0@gmail.com>
2023-04-11 17:04:07 +0200

    Added the ability for the server to send queries and return responses.

--- client.py
+++ client.py
@@ -1,11 +1,23 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 8080  # The port used by the server
+PORT = 8888  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    try:
+        s.connect((HOST, PORT))
+        while True:
+            command = input('Enter a command or use the "help" command to see the available options: ').encode('utf8')
+            s.sendall(command)
+            # Receiving responses from the server
+            response_data = s.recv(1024).decode('utf8')
+            print(response_data)
 
-print(f"Received {data!r}")
+            # Exit condition
+            if command.lower() == b'quit' or command.lower() == b'exit':
+                print('Exiting...')
+                break
+
+    except Exception as e:
+        print('Error: ', e)
+
--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 8080
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 8888  # The port used by the server
+
+COMMANDS = {
+    b'help': b'There will be help commands.',
+    b'info': b'There will be information.',
+}
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
+    s.listen(5)
+    print(f"Server listening on {HOST}:{PORT}...")
     conn, addr = s.accept()
+    print(f"Connected to client at {addr}")
     with conn:
-        print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            command = data.decode('utf8').strip().lower().encode('utf8')
+            response = COMMANDS.get(command, b'Unknown command')
+            conn.sendall(response)
